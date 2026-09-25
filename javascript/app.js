d90925100b1a688a4193cf3f20098766590c7da0
rickcham <rickchan07@gmail.com>
2026-09-25 10:33:11 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,9 @@
 // ---------- FUNÇÃO DE PROTEÇÃO DE PÁGINA ----------
 // Chame esta função no topo de dashboard.html, agenda.html e clientes.html.
 // Se o usuário não estiver logado, redireciona para login.html.
+
+  new window.VLibras.Widget('https://vlibras.gov.br');
+
 function exigirLogin() {
   const dados = localStorage.getItem('usuarioLogado');
   if (!dados) {
